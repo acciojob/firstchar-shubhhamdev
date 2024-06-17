08c7e56816ef0d0437e76050d50e1f7181e10584
Shubham Deogade <133051223+shubhhamdev@users.noreply.github.com>
2024-06-17 18:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function firstChar(text) {
-  // your code here
-	if (text.charAt(0) != ' ') {
-		return text.charAt(0);
-	}
-	return '';
+    // Check if the string is empty or only contains spaces
+    if (!text.trim()) {
+        return '';
+    }
+
+    // Iterate through the string until finding a non-space character
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i)!== ' ') {
+            return text.charAt(i);
+        }
+    }
+
+    // This line should never be reached since we return on the first non-space character found
+    return '';
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
+
+
